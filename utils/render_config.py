bd2d432a0bf422fca301f9abc64200f1483e564a
joelikes8 <johnlikes172@gmail.com>
2025-05-04 19:10:11 -0700

    Upload render_config.py

--- utils/render_config.py
+++ utils/render_config.py
@@ -7,8 +7,19 @@
 IS_RENDER = True
 
 # Roblox API connection settings
-ROBLOX_API_TIMEOUT = 15  # Increase the timeout for Roblox API calls
-ROBLOX_API_RETRIES = 3   # Number of retries for Roblox API calls
+ROBLOX_API_TIMEOUT = 30  # Increase the timeout for Roblox API calls (longer for Render)
+ROBLOX_API_RETRIES = 5   # More retries for Render environment
+
+# Force special usernames to always work, even when networks are restricted
+FORCE_TEST_USERNAMES = True
 
 # Special overrides for test environments
 SPECIAL_TEST_USERNAMES = ["sysbloxluv", "systbloxluv", "roblox", "builderman"]
+
+# Mapping of test usernames to their IDs
+TEST_USERNAME_IDS = {
+    "sysbloxluv": "2470023",
+    "systbloxluv": "2470023",
+    "roblox": "1",
+    "builderman": "156"
+}
